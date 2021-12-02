1082970e5f77f63ac7053193063f9af4729bf2c1
Bill Mårtensson <bill@magictechnology.se>
2021-12-02 09:18:45 +0100

    Initial Commit

---
+++ piaxjson/piaxjsonApp.swift
@@ -0,0 +1,17 @@
+//
+//  piaxjsonApp.swift
+//  piaxjson
+//
+//  Created by Bill Martensson on 2021-12-02.
+//
+
+import SwiftUI
+
+@main
+struct piaxjsonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
